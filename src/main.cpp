4e54b932e7374314de6f823f81fd6b350e380bab
mileta <mileta.jovanovic01@gmail.com>
2023-04-02 15:30:45 +0200

    dodat skybox

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,11 @@ unsigned int loadTexture(const char *path,  bool gammaCorrection);
 
 void drawPlane(Shader shader, unsigned int planeVAO, unsigned int floorTexture);
 
+unsigned int loadCubemap(vector<std::string> faces);
+
+void drawSkybox(Shader skyboxShader, glm::mat4 view, glm::mat4 projection, unsigned int skyboxVAO, unsigned int cubemapTexture);
+
+
 // settings
 const unsigned int SCR_WIDTH = 1600;
 const unsigned int SCR_HEIGHT = 900;
@@ -140,8 +145,8 @@ int main() {
         return -1;
     }
 
-    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
-    stbi_set_flip_vertically_on_load(true);
+    // tell stb_image.h not to flip loaded texture's on the y-axis (before loading model).
+    stbi_set_flip_vertically_on_load(false);
 
     programState = new ProgramState;
     //programState->LoadFromFile("resources/program_state.txt");
@@ -173,22 +178,55 @@ int main() {
             1.0f, -0.5f, -1.0f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f
     };
 
+    float skyboxVertices[] = {
+            // positions
+            -1.0f,  1.0f, -1.0f,
+            -1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+
+            -1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,
+
+            1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+
+            -1.0f, -1.0f,  1.0f,
+            -1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,
+
+            -1.0f,  1.0f, -1.0f,
+            1.0f,  1.0f, -1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            -1.0f,  1.0f,  1.0f,
+            -1.0f,  1.0f, -1.0f,
+
+            -1.0f, -1.0f, -1.0f,
+            -1.0f, -1.0f,  1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            -1.0f, -1.0f,  1.0f,
+            1.0f, -1.0f,  1.0f
+    };
+
     // configure global opengl state
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
-
-    // build and compile shaders
-    // -------------------------
-    Shader lightingShader("resources/shaders/model_lighting_bf.vs", "resources/shaders/model_lighting_bf.fs");
-
-    // load models
-    // -----------
-    Model egyptModel("resources/objects/egypt/objColumn.obj");
-    egyptModel.SetShaderTextureNamePrefix("material.");
-
-    // load textures
-    // -------------
-    unsigned int floorTexture = loadTexture(FileSystem::getPath("resources/textures/sand.jpg").c_str(), true);
+    //glDepthFunc(GL_LESS);
 
     // plane VAO
     unsigned int planeVAO, planeVBO;
@@ -204,6 +242,53 @@ int main() {
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
     glBindVertexArray(0);
+
+    // skybox VAO
+    unsigned int skyboxVAO, skyboxVBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glBindVertexArray(0);
+
+    // build and compile shaders
+    // -------------------------
+    Shader lightingShader("resources/shaders/model_lighting_bf.vs", "resources/shaders/model_lighting_bf.fs");
+    Shader skyboxShader("resources/shaders/skybox.vs", "resources/shaders/skybox.fs");
+
+    // load models
+    // -----------
+    Model egyptModel("resources/objects/egypt/objColumn.obj");
+    egyptModel.SetShaderTextureNamePrefix("material.");
+
+    // load textures
+    // -------------
+
+    unsigned int floorTexture = loadTexture(FileSystem::getPath("resources/textures/sand.jpg").c_str(), true);
+
+    vector<std::string> faces
+            {
+                    FileSystem::getPath("resources/textures/skybox/right.jpg"),
+                    FileSystem::getPath("resources/textures/skybox/left.jpg"),
+                    FileSystem::getPath("resources/textures/skybox/top.jpg"),
+                    FileSystem::getPath("resources/textures/skybox/bottom.jpg"),
+                    FileSystem::getPath("resources/textures/skybox/front.jpg"),
+                    FileSystem::getPath("resources/textures/skybox/back.jpg")
+            };
+    unsigned int cubemapTexture = loadCubemap(faces);
+
+
+    //shader configuration
+    //--------------------
+    lightingShader.use();
+    lightingShader.setInt("material.texture_diffuse1", 0);
+    lightingShader.setInt("material.texture_specular1", 1);
+
+    skyboxShader.use();
+    skyboxShader.setInt("skybox", 0);
 
     PointLight& pointLight = programState->pointLight;
     pointLight.position = glm::vec3(4.0f, 4.0, 0.0);
@@ -264,10 +349,10 @@ int main() {
                                                 (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = programState->camera.GetViewMatrix();
 
-        drawPlane(lightingShader, planeVAO, floorTexture);
-
         lightingShader.setMat4("projection", projection);
         lightingShader.setMat4("view", view);
+
+        drawPlane(lightingShader, planeVAO, floorTexture);
 
         // render egypt statue model
         glm::mat4 model = glm::mat4(1.0f);
@@ -276,6 +361,8 @@ int main() {
         model = glm::scale(model, glm::vec3(0.5f));    // it's a bit too big for our scene, so scale it down
         lightingShader.setMat4("model", model);
         egyptModel.Draw(lightingShader);
+
+        drawSkybox(skyboxShader, view, projection, skyboxVAO, cubemapTexture);
 
         if (programState->ImGuiEnabled)
             DrawImGui(programState);
@@ -293,8 +380,71 @@ int main() {
     ImGui::DestroyContext();
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
+    glDeleteVertexArrays(1, &planeVAO);
+    glDeleteVertexArrays(1, &skyboxVAO);
+    glDeleteBuffers(1, &planeVBO);
+    glDeleteBuffers(1, &skyboxVBO);
+
     glfwTerminate();
     return 0;
+}
+
+// loads a cubemap texture from 6 individual texture faces
+// order:
+// +X (right)
+// -X (left)
+// +Y (top)
+// -Y (bottom)
+// +Z (front)
+// -Z (back)
+unsigned int loadCubemap(vector<std::string> faces)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrChannels;
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
+}
+
+void drawSkybox(Shader skyboxShader, glm::mat4 view, glm::mat4 projection, unsigned int skyboxVAO, unsigned int cubemapTexture)
+{
+    glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+    //glDepthMask(GL_FALSE);
+
+    skyboxShader.use();
+    view = glm::mat4(glm::mat3(programState->camera.GetViewMatrix())); // remove translation from the view matrix
+    skyboxShader.setMat4("view", view);
+    skyboxShader.setMat4("projection", projection);
+
+    // skybox cube
+    glBindVertexArray(skyboxVAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0);
+    //glDepthMask(GL_TRUE);
+    glDepthFunc(GL_LESS); // set depth function back to default
 }
 
 void drawPlane(Shader lightingShader, unsigned int planeVAO, unsigned int floorTexture) {
